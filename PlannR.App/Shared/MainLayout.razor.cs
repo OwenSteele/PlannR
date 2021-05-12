d895182bfd068fbbd565bd9aad4d0f7d11ccd2a2
OwenSteele <owenfacebook@hotmail.co.uk>
2021-05-12 08:15:45 +0100

    Added styling to NavMenu and MainLayout views.

---
+++ PlannR.App/Shared/MainLayout.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using PlannR.App.Infrastructure.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannR.App.Shared
+{
+    public partial class MainLayout
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        private string NavMenuCssClass { get; set; }
+        protected void ToggleNavMenu()
+        {
+            if (NavMenuCssClass == "width: 300px;") NavMenuCssClass = "width: 0px;";
+            else NavMenuCssClass = "width: 300px;";
+        }
+    }
+}
---
+++ PlannR.App/Shared/NavMenu.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace PlannR.App.Shared
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+
+        private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
