e272e5c7882f8642912e7c67ec78acaa45c97969
OwenSteele <owenfacebook@hotmail.co.uk>
2021-05-20 20:34:32 +0100

    Added Accomodation Modal.

---
+++ PlannR.App/Pages/Modals/CreateAccomodationModal.razor.cs
@@ -0,0 +1,102 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using PlannR.App.Infrastructure.Contracts;
+using PlannR.App.Infrastructure.Services.Base;
+using PlannR.App.Infrastructure.ViewModels.Accomodation;
+using PlannR.App.Infrastructure.ViewModels.Nested;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannR.App.Pages.Modals
+{
+    public partial class CreateAccomodationModal
+    {
+        [CascadingParameter]
+        BlazoredModalInstance ModalInstance { get; set; }
+        [CascadingParameter]
+        public ModalParameters Parameters { get; set; }
+
+        [Inject]
+        public ITripDataService TripDataService { get; set; }
+        [Inject]
+        public IAccomodationDataService AccomodationDataService { get; set; }
+        [Inject]
+        public IAccomodationTypeDataService AccomodationTypeService { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public EditAccomodationViewModel EditAccomodationViewModel { get; set; }
+        [Parameter]
+        public ICollection<TripNestedViewModel> UserTrips { get; set; }
+        [Parameter]
+        public ICollection<AccomodationTypeNestedViewModel> AccomodationTypes { get; set; }
+        public bool Submitted { get; set; } = false;
+
+        public string Message { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            EditAccomodationViewModel = new EditAccomodationViewModel();
+            UserTrips = await TripDataService.GetTripNamesAsync();
+            AccomodationTypes = await AccomodationTypeService.GetAllTypeNamesAsync();
+        }
+
+        protected async Task HandleValidSubmit()
+        {
+            if (EditAccomodationViewModel.AccomodationId == Guid.Empty)
+            {
+                var result = await CreateAccomodationAsync();
+
+                await ModalInstance.CloseAsync(ModalResult.Ok(result));
+            }
+            else
+            {
+                await EditAccomodationAsync();
+
+                await ModalInstance.CloseAsync();
+            }
+        }
+
+        private async Task EditAccomodationAsync()
+        {
+            var response = await AccomodationDataService.UpdateAsync(EditAccomodationViewModel);
+
+            HandleResponse(response);
+        }
+
+        private async Task<Guid> CreateAccomodationAsync()
+        {
+            var response = await AccomodationDataService.CreateAsync(EditAccomodationViewModel);
+
+            HandleResponse(response);
+
+            return response.Context;
+        }
+
+        protected void HandleInvalidSubmit()
+        {
+            Message = "There are some validation errors. Please try again.";
+        }
+
+        private void HandleResponse(ApiResponse<Guid> response)
+        {
+            if (response.Successful)
+            {
+                Message = "Accomodation added successfully!";
+
+                Submitted = true;
+            }
+            else
+            {
+                Message = response.Message;
+
+                if (!string.IsNullOrEmpty(response.Errors)) Message += response.Errors;
+            }
+        }
+    }
+}
