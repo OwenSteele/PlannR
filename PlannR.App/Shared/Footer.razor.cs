41aa139e62920bc9406d7c707473573a24b117d7
OwenSteele <owenfacebook@hotmail.co.uk>
2021-06-02 17:31:18 +0100

    Changed footer when authorised.

---
+++ PlannR.App/Shared/Footer.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannR.App.Shared
+{
+    public partial class Footer
+    {
+        [Parameter]
+        public string FooterClass { get; set; } = "footer-wrapper";        
+    }
+}
